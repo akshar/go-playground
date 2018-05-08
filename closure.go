c6c61ce021421b98a8edff1c63de293b3c6d5ce6
Akshar Dave <akshar314@gmail.com>
2018-05-08 20:42:37 +0530

    closure

---
+++ closure.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func wrapper() func() int {
+	x := 0
+	return func() int {
+		x++
+		return x
+	}
+}
+
+func main() {
+	increment := wrapper()
+	fmt.Println(increment()) //1
+	fmt.Println(increment()) //2
+}
