9326f74bc86ed7d0bb332e6f65003c6c9b3e2212
Akshar Dave <akshar314@gmail.com>
2018-05-10 23:22:09 +0530

    factorial

--- variadic.go
+++ variadic.go
@@ -4,9 +4,13 @@ import "fmt"
 
 func main() {
 	fmt.Println(average(1, 2, 3, 4, 5, 6))
+
+	data := []float64{42,314,11,7}
+	fmt.Println(average(data...)) // 93.5
 }
 
 func average(nums ...float64) float64 {
+	fmt.Println(nums) // [1 2 3 4 5 6] type f64
 	var total float64
 	for _, v := range nums {
 		total += v
