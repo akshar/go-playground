ae5e36bf3acd8d256f140f5b2b62e766a5eae8be
Akshar Dave <akshar314@gmail.com>
2018-05-13 18:06:02 +0530

    use bufio

--- hashtable.go
+++ hashtable.go
@@ -1,10 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
@@ -13,7 +14,29 @@ func main() {
 		log.Fatalln(err)
 	}
 
-	bs, _ := ioutil.ReadAll(res.Body) // bs type []byte
-	str := string(bs)                 // string = slice of bytes and slice 0f bytes = rune, so convert to string
-	fmt.Println(str)
+	// bs, _ := ioutil.ReadAll(res.Body) // bs type []byte
+	// str := string(bs)                 // string = slice of bytes and slice 0f bytes = rune, so convert to string
+	// fmt.Println(str)
+	// OR scanner takes a  Reader
+
+	words := make(map[string]string)
+	sc := bufio.NewScanner(res.Body)
+	sc.Split(bufio.ScanWords)
+
+	for sc.Scan() {
+		words[sc.Text()] = ""
+	}
+	if err := sc.Err(); err != nil {
+		fmt.Println(os.Stderr, "read err")
+	}
+	i := 0
+
+	for k, _ := range words {
+		fmt.Println(k)
+		if i == 200 {
+			break
+		}
+		i++
+
+	}
 }
